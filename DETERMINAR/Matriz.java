562e99fb3377d873bcd8d1a8afbfbdb5d202ed5b
Juan Jose Porras <juanjoporras12@gmail.com>
2024-11-14 14:12:54 -0600

    Se agrega la clase Jugador

--- DETERMINAR/Matriz.java
+++ DETERMINAR/Matriz.java
@@ -14,11 +14,12 @@ public class Matriz {
     public Matriz() {
         sistema = new int[3][4];
     }
-    
-    //Setters y getters
+
+    // Setters y getters
     public int[][] getSistema() {
         return this.sistema;
     }
+
     public void setSistema(int[][] sistema) {
         this.sistema = sistema;
     }
@@ -29,14 +30,15 @@ public class Matriz {
 
         int tamañoFilas = this.sistema.length;
         int tamañoColumnas = this.sistema[0].length;
-        
-        do{     //Relleno la matriz de coeficientes
+
+        do { // Relleno la matriz de coeficientes
             for (int i = 0; i < tamañoFilas; i++) {
                 for (int j = 0; j < (tamañoColumnas - 1); j++) {
                     this.sistema[i][j] = randomNum.nextInt(-10, 10);
                 }
             }
-        }while(calcularDeterminante() == 0);    //Si el determinante de la matriz generada es cero, volver a realizar el proceso
+        } while (calcularDeterminante() == 0); // Si el determinante de la matriz generada es cero, volver a realizar el
+                                               // proceso
     }
 
     // Genera el vector de resultados
@@ -46,13 +48,13 @@ public class Matriz {
         int tamañoFilas = this.sistema.length;
         int ultimaColumna = this.sistema[0].length - 1;
         int[] resultados = new int[3];
-        do{     //Genero el vector de resultados
+        do { // Genero el vector de resultados
             for (int i = 0; i < tamañoFilas; i++) {
-                int random = randomNum.nextInt(-10, 10);    //Genero mi número aleatorio
-                this.sistema[i][ultimaColumna] = random;    //Agrego el número a mi sistema
-                resultados[i] = random;                     //Lo agrego a mi vector de resultados para su posterior análisis
+                int random = randomNum.nextInt(-10, 10); // Genero mi número aleatorio
+                this.sistema[i][ultimaColumna] = random; // Agrego el número a mi sistema
+                resultados[i] = random; // Lo agrego a mi vector de resultados para su posterior análisis
             }
-        }while(esNulo(resultados));    //Si el vector de resultados es nulo, volver a realizar el proceso
+        } while (esNulo(resultados)); // Si el vector de resultados es nulo, volver a realizar el proceso
     }
 
     // Para visualizar el sistema de ecuaciones
@@ -82,9 +84,9 @@ public class Matriz {
 
     // Verifica si un vector es nulo (todas sus entradas son cero)
     public boolean esNulo(int[] array) {
-        boolean nulo = true;    //Se asume que es nulo
+        boolean nulo = true; // Se asume que es nulo
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != 0) {    //Si alguna componente es distinta de cero, el vector no es nulo
+            if (array[i] != 0) { // Si alguna componente es distinta de cero, el vector no es nulo
                 nulo = false;
                 return nulo;
             }
@@ -92,46 +94,46 @@ public class Matriz {
         return nulo;
     }
 
-    public int calcularDeterminante(){
+    public int calcularDeterminante() {
         int[][] matrizSarrus = new int[3][5];
-        
-        //Copia el parametro y duplica la columna 1-2 en la 4-5 en un nuevo sistema.
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 5; j++){
-                if(j < 3){
-                    matrizSarrus[i][j] = this.sistema[i][j];    
-                }else{
-                    matrizSarrus[i][j] = this.sistema[i][j-3];
+
+        // Copia el parametro y duplica la columna 1-2 en la 4-5 en un nuevo sistema.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j < 3) {
+                    matrizSarrus[i][j] = this.sistema[i][j];
+                } else {
+                    matrizSarrus[i][j] = this.sistema[i][j - 3];
                 }
             }
         }
-        
-        //Se encarga de calcular el determinante
+
+        // Se encarga de calcular el determinante
         int producto = 1;
         int sumaUno = 0;
         int sumaDos = 0;
         int contador = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(i == 0){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 0) {
                     producto *= matrizSarrus[j][j];
-                }else if(i == 1){
-                    producto *= matrizSarrus[j][j+1];
-                }else{
-                    producto *= matrizSarrus[j][j+2];
+                } else if (i == 1) {
+                    producto *= matrizSarrus[j][j + 1];
+                } else {
+                    producto *= matrizSarrus[j][j + 2];
                 }
             }
             sumaUno += producto;
             producto = 1;
-            for(int j = 2; j >= 0; j--){
-                if(j == 2){
+            for (int j = 2; j >= 0; j--) {
+                if (j == 2) {
                     producto *= matrizSarrus[j][contador];
-                }else if(j == 1){
-                    producto *= matrizSarrus[j][contador+1];
-                }else{
-                    producto *= matrizSarrus[j][contador+2];
+                } else if (j == 1) {
+                    producto *= matrizSarrus[j][contador + 1];
+                } else {
+                    producto *= matrizSarrus[j][contador + 2];
                 }
-                
+
             }
             sumaDos += producto;
             producto = 1;
--- DETERMINAR/main.java
+++ DETERMINAR/main.java
@@ -1,49 +1,50 @@
+
 //Ingreso la librería para poder solicitarle datos al usuario
 import java.util.Scanner;
 //Ingreso la librería para poder limpiar la pantalla en plena ejecución
 import java.io.IOException;
+
 /**
  * Write a description of class main here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class main
-{
-    public static void main(String[] args) throws IOException, InterruptedException{
+public class main {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner input = new Scanner(System.in);
         Matriz matriz = new Matriz();
-        
         boolean generarNuevaMatriz = true;
-        while (generarNuevaMatriz){
-            matriz.generarMatrizCoeficientes();     //Se generan los coeficientes:
-            matriz.generarVectorResultados();       //Se genera el vector de resultados:
-            //Se muestran los resultados en pantalla:
+        while (generarNuevaMatriz) {
+            matriz.generarMatrizCoeficientes(); // Se generan los coeficientes:
+            matriz.generarVectorResultados(); // Se genera el vector de resultados:
+            // Se muestran los resultados en pantalla:
             System.out.println("Determinante:" + matriz.calcularDeterminante());
             matriz.visualizarSistema();
 
             boolean eleccionValida = false;
-            do{     //Ciclo para aceptar únicamente las respuestas esperadas en cuanto a generar otra matriz o no
+            do { // Ciclo para aceptar únicamente las respuestas esperadas en cuanto a generar
+                 // otra matriz o no
                 System.out.println("¿Desea generar otra matriz? (1:Si, 2:No)");
                 int eleccion = input.nextInt();
-                switch(eleccion){
+                switch (eleccion) {
                     case 1:
-                        matriz.setSistema(new int[3][4]);   //"Reinicio" el sistema para repetir el proceso
+                        matriz.setSistema(new int[3][4]); // "Reinicio" el sistema para repetir el proceso
                         eleccionValida = true;
-                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();   //Limpio la pantalla
+                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); // Limpio la pantalla
                         break;
                     case 2:
                         eleccionValida = true;
                         generarNuevaMatriz = false;
                         break;
                     default:
-                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();   //Limpio la pantalla
+                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); // Limpio la pantalla
                         System.out.println("Determinante:" + matriz.calcularDeterminante());
                         matriz.visualizarSistema();
                         System.out.println("\nPor favor ingrese una opción válida.\n");
                         break;
                 }
-            }while(!eleccionValida);
+            } while (!eleccionValida);
         }
     }
 }
